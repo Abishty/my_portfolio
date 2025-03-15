d99a77ba7882a08021237246c1c70bfb4dcd9cb3
Abishty <18abhishek.b@gmail.com>
2025-03-15 19:18:06 +0530

    Update layout and package files

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Outfit, Ovo } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const outfit = Outfit({
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
         className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
